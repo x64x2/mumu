9b2237ec70b90b4d3916152a9019aed75260a55d
cat/random/dev/erik <81354428+X64X2@users.noreply.github.com>
2024-03-27 02:31:13 +0000

    Delete dist directory

--- dist/validation.d.ts
+++
@@ -1,8 +0,0 @@
-import { Post } from './posts.js';
-declare const Validate: {
-    validationConfigDir: string;
-    post: (inp: Post) => void;
-    file: () => void;
-    filechunk: () => void;
-};
-export default Validate;
